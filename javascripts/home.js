197b0c69ff6b4834c4e2f3df7d9a640204bee7da
dylan/ <dilanefotio14@gmail.com>
2023-12-14 04:12:32 +0100

    last update of day

--- javascripts/home.js
+++ javascripts/home.js
@@ -1,1 +1,11 @@
+const menu = document.querySelector(".menu");
+const aside = document.querySelector("aside");
+const closeMenu = document.querySelector("#close");
 
+menu.addEventListener("click", () => {
+  aside.classList.toggle("max-sm:visible");
+});
+
+closeMenu.addEventListener("click", () => {
+  aside.classList.toggle("max-sm:visible");
+});
